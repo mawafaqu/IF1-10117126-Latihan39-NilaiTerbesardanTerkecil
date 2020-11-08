a05aa8f8575fb2520c6a78b72db6721c135da722
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-08 12:54:38 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan39-NilaiTerbesardanTerkecil/src/if1/pkg10117126/latihan39/nilaiterbesardanterkecil/IF110117126Latihan39NilaiTerbesardanTerkecil.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan39.nilaiterbesardanterkecil;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * Nama     : Mawa Faqu Rochman
+ * Kelas    : IF-1
+ * NIM      : 10117126
+ * Deskripsi Program : program ini berisi program nilai terbesar dan terkecil
+ * nilai mahasiswa 
+ */
+
+public class IF110117126Latihan39NilaiTerbesardanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         String namaPetugas;
+        int jmlMhs;
+
+        nilai hasil = new nilai();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===Program nilai terbesar dan nilai terkceil===");
+        System.out.print("Masukkan Nama Petugas : ");
+        namaPetugas = scanner.nextLine();
+        System.out.print("Masukkan Banyaknya Mahasiswa :  ");
+        jmlMhs = scanner.nextInt();
+        System.out.println();
+
+        hasil.inputNilai(jmlMhs);
+        System.out.println();
+        hasil.tampilNilaiTerbesarTerkecil(jmlMhs);
+
+        System.out.println("Nilai Terbesar : " + hasil.nilaiBesar);
+        System.out.println("Nilai Terkecil : " + hasil.nilaiKecil);
+        System.out.println();
+        System.out.println("Nama Petugas : " + namaPetugas);
+    }
+    
+}
